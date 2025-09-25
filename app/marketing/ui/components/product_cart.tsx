5f034196b02de78390b2b48efc654a955159ee8a
pandaguerrier <juleslofficial@gmail.com>
2025-09-25 10:57:03 +0200

    feat: a lot of things, restarted the project

--- app/marketing/ui/components/product_cart.tsx
+++ app/marketing/ui/components/product_cart.tsx
@@ -14,6 +14,7 @@ export function ProductCart({ product, quantity, size }: ProductCartProps) {
   const [actQantity, setActQuantity] = useState(quantity)
 
   const handleQuantityChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // @ts-ignore
     const value = parseInt(e.target.value)
     if (value <= stockQuantity && value >= 0) {
       setActQuantity(value)
